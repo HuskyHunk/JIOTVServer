4dea73ba8aacf87b519f5a787391abda8b2859c2
HuskyHunk <92379169+HuskyHunk@users.noreply.github.com>
2023-02-05 06:44:42 +0530

    Update install.sh

--- V2.0.3/install.sh
+++ V2.0.3/install.sh
@@ -22,7 +22,7 @@ else
 fi
 echo "downloading script"
 # Get the latest release of code
-curl https://cdn.discordapp.com/attachments/971776543229632534/1071422927561568327/JTVServer.zip -o JTVServer.zip
+curl https://raw.githubusercontent.com/HuskyHunk/JIOTVServer/main/V2.0.3/JTVServer.zip -o JTVServer.zip
 
 
 if [ $? -eq 0 ]; then
@@ -52,11 +52,11 @@ fi
 
 echo "Downloading start server script"
 cd ~
-curl -o start.sh https://raw.githubusercontent.com/dhruv-2015/JIOTVServer/main/V2.0.3/start.sh
+curl -o start.sh https://raw.githubusercontent.com/HuskyHunk/JIOTVServer/main/V2.0.3/start.sh
 if [ $? -eq 0 ]; then
    echo "Script downloaded successfully"
 else
-    echo "Failed to download start script. Please run wget https://raw.githubusercontent.com/dhruv-2015/JIOTVServer/main/V2.0.3/start.sh to download"
+    echo "Failed to download start script. Please run wget https://raw.githubusercontent.com/HuskyHunk/JIOTVServer/main/V2.0.3/start.sh to download"
     exit 1
 fi
 
